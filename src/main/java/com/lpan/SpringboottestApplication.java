e7f51f3df51b5a2dc96b1120892a403e99588a54
Lpan <span4718@163.com>
2018-01-16 17:33:00 +0800

    注解

--- src/main/java/com/lpan/SpringboottestApplication.java
+++ src/main/java/com/lpan/SpringboottestApplication.java
@@ -1,9 +1,14 @@
 package com.lpan;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
+//@SpringBootAplication相当于    @Configuration，@@EnableAutoConfiguration，@CompmentScan
 public class SpringboottestApplication {
 
 	public static void main(String[] args) {

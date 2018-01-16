69277f1396b80d0b7c4cd1e4c9aa5fc96316f581
Lpan <span4718@163.com>
2018-01-16 17:35:25 +0800

    注解

--- src/main/java/com/lpan/LpanController.java
+++ src/main/java/com/lpan/LpanController.java
@@ -2,6 +2,7 @@ package com.lpan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,19 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by Lpan on 2018/1/16.
  */
-//@RestController
-@Controller
-@EnableAutoConfiguration
+@RestController
 public class LpanController {
 
     @RequestMapping("/lpan")
-    @ResponseBody
-    public String Lpan(){
+    public String Lpan() {
         System.out.println("123");
         return "Lpan";
-    }
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(LpanController.class, args);
     }
 }
